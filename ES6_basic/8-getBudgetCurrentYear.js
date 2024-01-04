fa20eaff265920bd1f3e2c89945c81c207126a28
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-04 10:34:24 -0600

    use computed property

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,4 +1,4 @@
-function getCurrentYear () {
+function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
@@ -7,7 +7,7 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
     [`income_${getCurrentYear()}`]: income,
     [`gdp_${getCurrentYear()}`]: gdp,
-    [`capita_${getCurrentYear()}`]: capita
+    [`capita_${getCurrentYear()}`]: capita,
   };
   return budget;
 }
