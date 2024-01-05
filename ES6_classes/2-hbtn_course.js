22c5128ba6cda025970506c68073bfeac127f3b0
Frandy Slueue <frandy4ever@gmail.com>
2024-01-05 15:20:25 -0600

    update

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,37 +1,37 @@
-import class HolbertonCourse {
-    constructor(name, length, students) {
-        if (typeof name === 'string' || typeof length === 'number' || !Array.isArray(students)) {
-            throw new TypeError('Invalid attribute type');
-        }
-        this._name = name;
-        this._length = length;
-        this._students = students;
+class HolbertonCourse {
+  constructor (name, length, students) {
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new TypeError('Invalid attribute type');
     }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    get privateName() { return this._name; }
-    
-    set privateName(newName) {
-        if (typeof newName !== 'string'){
-            throw new TypeError('Invalid type for newName');
-        }
-        this._name = newName;
-    }
-    
-    get privateLength() { return this._length; }
-    
-    set privateLength(newLength) {
-        if (typeof newLength !== 'number') {
-            throw new TypeError('Invalid type for newLength');
-        }
-        this._name = newLength;
-    }
-    
-    get privateStudents() { return this._students; }
+  get privateName () { return this._name; }
 
-    set privateStudents(newStudents) {
-        if (!Array.isArray(newStudents)) {
-            throw new TypeError('Invalid type for newStudents');
-        }
-        this._name = newStudents;
+  set privateName (newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Invalid type for newName');
     }
+    this._name = newName;
+  }
+
+  get privateLength () { return this._length; }
+
+  set privateLength (newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Invalid type for newLength');
+    }
+    this._length = newLength;
+  }
+
+  get privateStudents () { return this._students; }
+
+  set privateStudents (newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Invalid type for newStudents');
+    }
+    this._students = newStudents;
+  }
 }
