182fb639bb2a9a8d1721e3f4297adfcb19159a5a
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-05 03:46:59 -0600

    fix indentation issur

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
 
   for (const idx of array) {
-  newArray.push(appendString + idx);
+    newArray.push(appendString + idx);
   }
   return newArray;
 }
