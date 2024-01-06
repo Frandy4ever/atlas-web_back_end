f5dc8f7a181ed06518446575551616aaee82424a
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-06 00:02:13 -0600

    update

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -11,7 +11,6 @@ export class HolbertonClass {
   get location() {
     return this._location;
   }
-
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
@@ -36,7 +35,6 @@ export class StudentHolberton {
   }
 }
 
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
@@ -44,3 +42,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
