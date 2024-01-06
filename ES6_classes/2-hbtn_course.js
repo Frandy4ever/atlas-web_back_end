438d59012d52a64d63fd7cc4887c5ab771fa1a98
Frandy Slueue <frandy4ever@gmail.com>
2024-01-05 18:50:29 -0600

    update

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,10 +2,11 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string' && typeof length !== 'number' && !Array.isArray(students)) {
       throw new TypeError('wrong type');
+    } else {
+      this._name = name;
+      this._length = length;
+      this._students = students;
     }
-    this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
   get privateName() {
