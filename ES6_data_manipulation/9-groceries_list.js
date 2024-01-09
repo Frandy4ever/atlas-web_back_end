bd4fa55d8e6eec3bdafba109e96afdadad87b288
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-09 14:12:21 -0600

    create needed files for ES6_promise dir

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,12 +1,1 @@
 export default function groceriesList(name, quantity) {
-  if (!(groceries instanceof Map)) {
-    throw new TypeError('Cannot process');
-  }
-  const groceries = new Map();
-  groceries.set('Apples', 10);
-  groceries.set('Tomatoes', 10);
-  groceries.set('Pasta', 1);
-  groceries.set('Rice', 1);
-  groceries.set('Banana', 5);
-  return groceries;
-}
