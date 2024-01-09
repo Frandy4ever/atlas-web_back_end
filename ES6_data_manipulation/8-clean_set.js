4b0f70019c6322aba3d1700224eedd23307fc47f
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-09 12:44:46 -0600

    update

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
-  const filteredValue = Array.from(inputSet).filter(value => value.startsWith(startString).join('-'));
-
-  return filteredValue;
+  if (!(set instanceof Set)) {
+    throw new TypeError('Expected a Set');
+  }
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+  return Array.from(set).filter((item) => typeof item === 'string' && item.startsWith(startString)).map((item) => item.slice(startString.length)).join(' ');
 }
