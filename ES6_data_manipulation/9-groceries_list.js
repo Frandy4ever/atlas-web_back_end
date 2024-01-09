e1dec9be396d0d1eadc6d15d7d2cb016ca0ceb5b
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-09 15:32:38 -0600

    update

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,11 @@
-export default function groceriesList(name, quantity) {
+export default function groceriesList(names, quantities) {
+  const groceryMap = new Map();
+
+  if (!Array.isArray(names) || !Array.isArray(quantities) || names.length !== quantities.length) {
+    throw new Error('Invalid arguments');
+  }
+  for (const [name, quantity] of names.entries()) {
+    groceryMap.set(name, quantities[quantity]);
+  }
+  return groceryMap;
+}
