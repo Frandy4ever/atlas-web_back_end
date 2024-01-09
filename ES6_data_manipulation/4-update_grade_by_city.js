e7b14450a0cdcc4823a239888577e88cb9ae474d
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-08 23:19:40 -0600

    update

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,12 @@
+import getListStudents from './0-get_list_students';
+
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.filter((student) => student.location === city).map((student) => {
+  const highOrder = students.filter((student) => student.location === city).map((student) => {
     const newGrade = newGrades.find((grade) => grade.studentId === student.id);
     if (newGrade) {
       return { ...student, grade: newGrade.grade };
     }
   });
+
+  return highOrder;
 }
