a256ea3b2dcf7832148dc64f6356c7a963e1f09a
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-09 13:42:19 -0600

    hard coding

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -2,6 +2,11 @@ export default function groceriesList(name, quantity) {
   if (!(groceries instanceof Map)) {
     throw new TypeError('Cannot process');
   }
-  groceries.set(name, quantity);
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
   return groceries;
 }
