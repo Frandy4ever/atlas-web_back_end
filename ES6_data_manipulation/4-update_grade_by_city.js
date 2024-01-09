2e9df68554b99ccddec204fcec96c2266de8f11c
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-09 06:40:19 -0600

    remove the use of find()

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,11 @@
 import getListStudents from './0-get_list_students';
 
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  const highOrder = students.filter((student) => student.location === city).map((student) => {
-    const newGrade = newGrades.find((grade) => grade.studentId === student.id);
-    if (newGrade) {
-      return { ...student, grade: newGrade.grade };
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  return listStudents.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+    if (grade[0]) {
+      return { ...student, grade: grade[0].grade };
     }
+    return { ...student, grade: 'N/A' };
   });
-
-  return highOrder;
 }
