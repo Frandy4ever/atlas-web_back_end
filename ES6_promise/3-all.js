3fb368918622fbb0e7b8893c787b68f1db5f4d3c
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-10 17:49:24 -0600

    update

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
     })
     .catch(() => console.log('Signup system offline'));
 }
