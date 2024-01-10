001873e66b5a6bdc8c1bafea83a3574edeb9041c
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-10 17:23:47 -0600

    update

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,38 +1,9 @@
-export function uploadPhoto() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        status: 200,
-        body: 'photo-profile-1',
-      });
-    }, 1000);
-  });
-}
+import {uploadPhoto, createUser} from './utils';
 
-export function createUser() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        firstName: 'Guillaume',
-        lastName: 'Salva',
-      });
-    }, 1000);
-  });
-}
-
-import { uploadPhoto, createUser } from './utils';
-
-function handleProfileSignup() {
-  const promises = [uploadPhoto(), createUser()];
-
-  Promise.all(promises)
-    .then((results) => {
-      const [photoResponse, userResponse] = results;
-      console.log(`body: ${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
-    .catch((error) => {
-      console.error('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-
-handleProfileSignup();
