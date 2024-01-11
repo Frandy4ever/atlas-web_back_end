c17b80d474cde65fd0c182f8f9334872da128a0d
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-10 19:21:21 -0600

    fix unexpected block statement in arrow body

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      return results.map((result) => {
+      results.map((result) => {
         if (result.status === 'fulfilled') {
           return { status: result.status, value: result.value };
         }
