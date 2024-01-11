a9a286d89aadbd688074532878552ade79aa10ab
Frandy G Slueue <frandy4ever@gmail.com>
2024-01-10 19:24:21 -0600

    update

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,7 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {results.map((result) => {
+    .then((results) => {
+      results.map((result) => {
         if (result.status === 'fulfilled') {
           return { status: result.status, value: result.value };
         }
