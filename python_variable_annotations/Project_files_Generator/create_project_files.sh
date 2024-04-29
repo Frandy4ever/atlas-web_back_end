f7651c59e826770486c129bc1fa1fa3ee3bac186
Frandy Slueue <frandy4ever@gmail.com>
2024-04-29 03:32:43 -0500

    update folder name

---
+++ python_variable_annotations/Project_files_Generator/create_project_files.sh
@@ -0,0 +1,7 @@
+# Create all project files and add executable previlleges
+
+# for file in '0-add.py' '1-concat.py' '2-floor.py' '3-to_str.py' '4-define_variables.py' '5-sum_list.py' '6-sum_mixed_list.py' '7-to_kv.py' '8-make_multiplier.py' '9-element_length.py'; do
+#     touch "$file"
+#     chmod +x "$file"
+#     echo '#!/usr/bin/env python3' > "$file"
+# done
