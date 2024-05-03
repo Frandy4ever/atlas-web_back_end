27eed040eeb65054babb304e6582528da7221271
Frandy Slueue <frandy4ever@gmail.com>
2024-05-02 19:55:54 -0500

    add two floating numbers

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+  """
+  Add two floats => result.
+
+  Args:
+    a (float): fist number.
+    b (float): second number.
+
+  Returns:
+    float: sum of a and b.
+  """
+  return a + b
