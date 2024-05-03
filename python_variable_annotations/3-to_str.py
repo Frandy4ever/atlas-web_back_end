4c57d56a2894b5106b355603b3f83a4443c9c5da
Frandy Slueue <frandy4ever@gmail.com>
2024-05-02 20:44:57 -0500

    convert float to string

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+Function which converts a given float into a string
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    Return string rep of input float n.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: string rep of the input float n.
+    """
+    return str(n)
