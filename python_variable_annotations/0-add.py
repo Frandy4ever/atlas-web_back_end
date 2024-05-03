f37074d5b6325432fd8aebf3b8d4931a1000caed
Frandy Slueue <frandy4ever@gmail.com>
2024-05-02 20:01:15 -0500

    add documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
+"""
+Add two float numbers
+"""
 
 def add(a: float, b: float) -> float:
-  """
-  Add two floats => result.
+    """
+    Add two floats => result.
 
-  Args:
-    a (float): fist number.
-    b (float): second number.
+    Args:
+        a (float): fist number.
+        b (float): second number.
 
-  Returns:
-    float: sum of a and b.
-  """
-  return a + b
+    Returns:
+        float: sum of a and b.
+    """
+    return a + b
