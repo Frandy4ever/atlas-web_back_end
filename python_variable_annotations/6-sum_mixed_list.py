230fc5620e07ae5b4a8c5812152d835d73f5fe06
Frandy Slueue <frandy4ever@gmail.com>
2024-05-02 21:29:41 -0500

    get the sum of list of floats and ints

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+"""
+Return the sum of floats and ints as float.
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Calc the sum of elements in list of ints and floats.
+
+    Args:
+        mxd_lst (List[Union[int, float]]): A list of integers and floats.
+
+    Returns:
+        float: The sum of elements in the input list.
+    """
+    return sum(mxd_lst)
