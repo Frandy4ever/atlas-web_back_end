8917a8027635d9db0a6d6031e254b5208fe74187
Frandy Slueue <frandy4ever@gmail.com>
2024-05-02 20:20:22 -0500

    return float number as floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+Return a float number as floor.
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Return n as floor
+
+    Args:
+        n (float): number to return as floor
+
+    Returns:
+        floor: mutate float n to floor
+    """
+    return math.floor(n)
