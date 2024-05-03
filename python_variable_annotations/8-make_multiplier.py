af18d3fe053439ce7269542275896cedc32c4347
Frandy Slueue <frandy4ever@gmail.com>
2024-05-03 07:15:44 -0500

    create multiplier fn

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+"""Return a multiplier function"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Function that multiplies a float by a given multiplier
+
+    Args:
+        multiplier (float): multiplier value.
+
+    Returns:
+        Callable[[float], float]: A function that multiplies a float by a given multiplier
+    """
+    
