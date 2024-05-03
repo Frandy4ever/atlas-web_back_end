af82f51706953d4a0945cdef500c5a198c42b80c
Frandy Slueue <frandy4ever@gmail.com>
2024-05-03 07:53:23 -0500

    update import statement, adjust documentations, and update fn definition

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
 """
-List of tuple containing each element
-from the input list and its length
+Calc length of elements in the list
 """
 
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Return a list of tuples containing each element from
     the input list and its length.
 
     Args:
-        lst (list[tuple[str, int]]): 
-            List of tuples where each contains an element
-            from `lst` and its corresponding length.
+        lst (Iterable[Sequence]): input iterable of sequence.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples where each
+        tuple contains an element from the input
+        iterable and its corresponding length.
     """
     return [(i, len) for i in lst]
