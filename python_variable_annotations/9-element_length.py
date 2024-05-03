a1c4c12d5e4836350dc14349ccb6f9119667ac4f
Frandy Slueue <frandy4ever@gmail.com>
2024-05-03 07:44:56 -0500

    add missing import statement and fix funciton defination

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,4 +5,17 @@ List of tuple containing each element
 from the input list and its length
 """
 
+from typing import List, Tuple
 
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    Return a list of tuples containing each element from
+    the input list and its length.
+
+    Args:
+        lst (list[tuple[str, int]]): 
+            List of tuples where each contains an element
+            from `lst` and its corresponding length.
+    """
+    return [(i, len) for i in lst]
