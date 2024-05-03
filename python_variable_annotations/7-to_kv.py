00065e9259b19e177314d21909ebbaf93dd6bae8
Frandy Slueue <frandy4ever@gmail.com>
2024-05-02 21:48:27 -0500

    update fn spacing

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,6 +6,7 @@ Return a tuple of the inputs string k and float or integer v
 
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Create a tuple, first elm is string k, and second elm is 
