dd6509c3967d927bf4ba5c1d681e52ec3ff84b49
Frandy Slueue <frandy4ever@gmail.com>
2024-05-05 08:20:02 -0500

    measure total execution time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+"""Measures total execution time."""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the average execution time for spawning n
+    coroutines with a maximum delay.
+
+    Args:
+        n (int): The number of coroutines to spawn.
+        max_delay (int): The maximum delay value for each coroutine.
+
+    Returns:
+        float: The average execution time per coroutine in seconds.
+    """
+
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
