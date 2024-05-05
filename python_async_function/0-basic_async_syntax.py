78ecc4e810eeb1b2139232b7a373a1ea5c101ebd
Frandy Slueue <frandy4ever@gmail.com>
2024-05-04 23:29:12 -0500

    remove trailing whitespaces

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 """
-Async coroutine that takes in a random waiting time 
+Async coroutine that takes in a random waiting time
 as integer and return float.
 """
 import random, asyncio
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
-    This function generates a random number between 0 
+    This function generates a random number between 0
     and a received max number to use as coroutine delay timer.
 
     Args:
