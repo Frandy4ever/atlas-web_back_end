302d83b2be24d3d48c6e5ec601ef5eacb663dff2
Frandy Slueue <frandy4ever@gmail.com>
2024-05-04 23:26:06 -0500

    remove calling code line

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -20,10 +20,3 @@ async def wait_random(max_delay: int = 10) -> float:
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-async def main() -> None:
-    """Main fn used to run our wait_random() fn"""
-    delay: float = await wait_random()
-    print(delay)
-
-asyncio.run(main())
