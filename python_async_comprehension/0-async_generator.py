83bfcc78b0f7d8fab5eff8280dabe7b1cb2076a0
Frandy Slueue <frandy4ever@gmail.com>
2024-05-05 23:22:06 -0500

    create a fn that takes no args, runs a coroutine 10 times, waits a sec then generates random number between 0 and 10

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+"""
+This script takes on no arguments, runs a coroutine 10 times, 
+asynchronously waits 1sec, then yield a random number 0 through 10.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Async generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
