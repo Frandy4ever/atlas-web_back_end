8693e65046e7d52ca9936f61c8e107472eb0ac81
Frandy Slueue <frandy4ever@gmail.com>
2024-05-05 20:36:53 -0500

    create an asyncio task from an integer

--- python_async_function/3-task.py
+++ python_async_function/3-task.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+"""
+This fn uses a received integer to create an asyncio task.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_dalay: int) -> asyncio.Task:
+    """
+    Return asyncio task from a given integer.
+
+    Args:
+        max_delay (int): integer used for creating asyncio task.
+
+    Returns:
+        asyncio.task
+    """
+    task = asyncio.create_task(wait_random(max_dalay))
+    return task
