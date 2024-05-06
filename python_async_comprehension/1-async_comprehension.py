03f8b5b59d2cb88f16d97c4cd74ecf88d4056803
Frandy Slueue <frandy4ever@gmail.com>
2024-05-05 23:54:35 -0500

    write a fn that randomly returns collected numbers

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+"""
+This fn takes no args, it imports async_generator,
+runs a coroutine which collects 10
+random numbers using async comprehension
+then return 10 random numbers
+"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[int]:
+    """Returns the randomly collected numbers"""
+    return [num async for num in async_generator()]
