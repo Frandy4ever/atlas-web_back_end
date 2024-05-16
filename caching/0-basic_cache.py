6d17f8389b11b3d7e56b717c7baf03e5a3c10beb
Frandy Slueue <frandy4ever@gmail.com>
2024-05-16 10:04:13 -0500

    class that inhenrits from a no limit caching system

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Creates a basic dictionary"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Inherits from a no limit caching system"""
+    def get(self, key):
+        """Dict getter"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        
+        return self.cache_data.get(key)
+
+
+    def put(self, key, item):
+        """Dict setter"""
+        if key and item:
+            self.cache_data[key] = item
