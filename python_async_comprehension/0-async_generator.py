f90b3d67d5eaf80eba6a79669189ce422a98b899
Frandy Slueue <frandy4ever@gmail.com>
2024-05-16 08:51:39 -0500

    update number of params

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,8 +9,8 @@ from typing import AsyncGenerator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Async generator"""
-    for _ in range(11):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
