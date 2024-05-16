daf4293b3882378087f646d92922e92705ededf8
Frandy Slueue <frandy4ever@gmail.com>
2024-05-16 11:15:23 -0500

    Update index_range function to specify return type using Python typing

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+"""This is a helper fn"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple containing the start and end indexes for pagination.
+
+    Args:
+        page (int): The page number.
+        page_size (int): The size of each page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start index and the end index
+        corresponding to the range of indexes to return in a list for the
+        given pagination parameters.
+    """
+    if page and page_size:
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
