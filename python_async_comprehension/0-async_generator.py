b8d12025a0e7bd49fee3b74e9c92168cea4a6cf5
Frandy Slueue <frandy4ever@gmail.com>
2024-05-16 08:54:59 -0500

    change to Generator instead of AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,11 +5,11 @@ This script takes on no arguments, runs a coroutine 10 times,
 asynchronously waits 1sec, then yield a random number 0 through 10.
 """
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
