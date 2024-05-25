30e0e72b8a3174dadde63b55cc60dfe0802240cf
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-25 10:42:46 -0500

    filter values of log records

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,26 @@
 the log message obfuscated."""
 import re
 from typing import List
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Constructor fn"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values of incoming log records."""
+        return filter_datum(self.fields, self.REDACTTION,
+                            super().format(record), self.SEPARATOR)
 
 def filter_datum(fields: List[str],
                  redaction: str,
