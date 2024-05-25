937b9317cb42c6b4b5244980d8e1e3f12d7cbd33
Frandy Slueue <Frandy4ever@gmail.com>
2024-05-25 10:27:23 -0500

    update code structure

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""This program will call filter_datum that returns
+the log message obfuscated."""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    for i in fields:
+        message = re.sub(fr'{i}=.+?{separator}',
+                         f'{i}={redaction}{separator}', message)
+    return message
