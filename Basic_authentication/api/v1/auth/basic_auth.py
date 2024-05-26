4e2dcd6beaf509d183bf17c6cda54725c57b1605
Frandy Slueue <frandy4ever@gmail.com>
2024-05-26 01:27:45 -0500

    return use instance based on their information

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,19 +41,15 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
-        '''Returns user instance base on their info'''
+        """ returns the User instance based on his email and password """
         if user_email is None or not isinstance(user_email, str):
             return None
-
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-
         try:
             users = User.search({'email': user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
         except Exception:
             return None
-
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
