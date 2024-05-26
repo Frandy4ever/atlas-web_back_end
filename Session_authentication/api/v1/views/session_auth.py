ff4e722ff78750006fe56e4f0013c778b8b07f88
Frandy Slueue <frandy4ever@gmail.com>
2024-05-26 16:45:16 -0500

    create a logout path

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -2,9 +2,9 @@
 
 """ handles all routes for the views for Session authentication """
 from api.v1.views import app_views
-from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
+from flask import abort, jsonify, request
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -28,3 +28,15 @@ def login() -> str:
             return user_json
         else:
             return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """ view for route /auth_session/logout, method DELETE """
+    from api.v1.app import auth
+    destroy_session = auth.destroy_session(request)
+    if destroy_session is False:
+        abort(404)
+    else:
+        return jsonify({}), 200
