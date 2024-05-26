7463c04cafe79f3e2722229c6f783b3334bfd5b2
Frandy Slueue <frandy4ever@gmail.com>
2024-05-26 00:16:37 -0500

    create a class to manage API authentication

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""This program is authentication management module"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''Manages API authentications.'''
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''Reqired authentications.'''
+        if not path or not excluded_paths:
+            return True
+
+        path += '/' if path[-1] != '/' else ''
+        wildcard = any(rex.endswith("*") for rex in excluded_paths)
+
+        if not wildcard:
+            if path in excluded_paths:
+                return False
+
+        for rex in excluded_paths:
+            if rex[-1] == '*':
+                if path.startswith(rex[:-1]):
+                    return False
+            if rex == path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''Handles authorizations.'''
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''Manages current user.'''
+        return None
