eb82ffa3cc4dfe42ee446c97e19528a6ed501cf6
Frandy Slueue <frandy4ever@gmail.com>
2024-05-26 16:09:56 -0500

    create session cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -38,3 +38,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         '''Manages current user.'''
         return None
+    
+    def session_cookie(self, request=None):
+        """ returns a cookie value from a request """
+        if request is None:
+            return None
+        session_name = getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
