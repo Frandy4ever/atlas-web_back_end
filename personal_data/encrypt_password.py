b2612d5fa344367d0986672fefa508a348d5be38
Frandy Slueue <frandy4ever@gmail.com>
2024-05-25 23:07:59 -0500

    encrypt passwords

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""This program will encrypt passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Return a salted hashed password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
