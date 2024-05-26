c3fde741b9ae54e0f562a3220561c4391df4ff65
Frandy Slueue <frandy4ever@gmail.com>
2024-05-26 01:33:28 -0500

    return use instance based on their information

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""This program """
+"""This program handles all basic authentications"""
 from api.v1.auth.auth import Auth
 from base64 import b64decode
-import base64
-import binascii
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
