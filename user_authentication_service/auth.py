e1bb5355768462bf225ad63948e446c1258e4178
Frandy Slueue <frandy4ever@gmail.com>
2024-05-26 17:42:55 -0500

    register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -8,3 +8,22 @@ def _hash_password(password: str) -> str:
     '''self descriptive'''
     salted_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return salted_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        """ constructor """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Returns user object with mandatory email and password verification """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, pwd)
+            return user
+        else:
+            raise ValueError('User {email} already exists')
