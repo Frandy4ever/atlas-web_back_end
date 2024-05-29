565524318d657c0974ec9f67946e20b37f7e982a
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 12:33:58 -0500

    create TestAccessNestedMap class and test_access_nested_map method with @parameterized decorator

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
-
-"""This program is a Unittest"""
+""" Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
 import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
 
-
+class TestAccessNestedMap(unittest.TestCase):
+    """ TESTCASE """
+    """ Testing nested map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        ''' Test access nested map '''
+        self.assertEqual(access_nested_map(nested_map, path), expected)
