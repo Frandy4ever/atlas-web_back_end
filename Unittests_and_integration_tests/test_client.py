c419833a8982fbb28e78585c4da46853ad231f17
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 13:08:54 -0500

    create TestGithubOrgClient class and implement test_org method

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Unit test for Testing client"""
+
+import unittest
+import json
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from urllib.error import HTTPError
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ TESTCASE """
+    """ Test functionalities """
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+        ])
+    @patch("client.get_json", return_value={"payload": True})
+    def test_org(self, org_name, mock_get):
+        """ test that GithubOrgClient.org returns the correct value """
+        test_client = GithubOrgClient(org_name)
+        test_return = test_client.org
+        self.assertEqual(test_return, mock_get.return_value)
+        mock_get.assert_called_once
+
