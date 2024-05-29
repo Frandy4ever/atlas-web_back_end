67411580b6784037df26db992ab886330bed7859
Frandy Slueue <frandy4ever@gmail.com>
2024-05-29 12:39:42 -0500

    create test_access_nested_mat_eception method and test for keyError

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         ''' Test access nested map '''
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    """ Testing for raised KeyError """
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ method to test that a KeyError is raised properly """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertEqual(error.exception.args[0], path[-1])
