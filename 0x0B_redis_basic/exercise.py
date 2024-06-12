9fbd0a8479295823647cad69861c9e7b4fe45e75
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 14:13:32 -0500

    implement get method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -9,7 +9,7 @@ from typing import Union, Optional, Any, Callable
 class Cache:
     """ class """
     def __init__(self):
-        """ constructor - store an instance of the Redis client as a private
+        """ Constructor - store an instance of the Redis client as a private
         variable named _redis and flush the instance using flushdb """
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -22,3 +22,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Callable will be used to convert data back to the desire format."""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Parametrize Cache.get to str """
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """Parametrize Cache.get to int """
+        data = self._redis.get(key)
+        try:
+            data = int(value.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
