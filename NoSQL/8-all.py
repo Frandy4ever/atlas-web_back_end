0cd0bf9c3eec1343db5ade20c19290cdca81c5ca
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 18:52:43 -0500

    List documents in a collection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+""" List all Python docs """
+import pymongo
+
+
+def list_all(mongo_collection):
+    """ Lists documents in a collection """
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
