b85a6f407b8bbfcc3fb0b91bb0a351e7ee4bc41b
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 14:01:09 -0500

    create cache class with store method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+"""Writing strings to Redis"""
+import redis
+import uuid
+
+
+class Cache:
+    """ class """
+    def __init__(self):
+        """ constructor - store an instance of the Redis client as a private
+        variable named _redis and flush the instance using flushdb """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ With uuid, generate random key, store the input data in
+        Redis using the random key and return the key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
