5d1ce26f4cba62e978886a6711794ce6427de06f
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 15:32:53 -0500

    Creates a function SafeDiv that divides

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Creates a function SafeDiv that divides (and returns)
+DELIMITER |
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+BEGIN
+  DECLARE result FLOAT;
+  IF b = 0 THEN
+    SET result = 0;
+  ELSE
+    SET result = a / b;
+  END IF;
+  RETURN result;
+END;
+|
