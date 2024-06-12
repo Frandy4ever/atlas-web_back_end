4ab1797850d4caae53911cade198cd52fbb26dd7
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 18:57:53 -0500

    python doc insertion

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+""" Python document insertion """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Inserts a new document in a collection based on kwargs,
+    Returns the new _id """
+    return mongo_collection.insert_one(kwargs).inserted_id
