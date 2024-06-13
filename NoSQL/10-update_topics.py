83c1221775ca635e7f037051ec8fc298f3ee6417
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 19:00:41 -0500

    update all topics of a school doc

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Update school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates all topics of a school document based on the name """
+    return mongo_collection.update_many({ "name": name }, { "$set": { "topics": topics } })
