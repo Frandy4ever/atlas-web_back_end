f920b8712435fa6e059a90cb94d635574f9967b3
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 19:08:31 -0500

    log all stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""Log all stats"""
+from pymongo import MongoClient
+
+
+def helper(a: dict) -> int:
+    """Client log"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(a)
+
+
+def main():
+    """ Provide stats about Nginx logs stored in MongoDB """
+    print(f"{helper({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {helper({'method': 'GET'})}")
+    print(f"\tmethod POST: {helper({'method': 'POST'})}")
+    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
+    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()
