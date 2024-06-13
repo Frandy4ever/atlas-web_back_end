497d06a4b41faf1cc5f20cd614e7ac378c77e3e0
Frandy Slueue <frandy4ever@gmail.com>
2024-06-12 19:05:13 -0500

    Returns list of school with a specific topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+""" Learing Python """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ Returns list of school with a specific topic """
+    return mongo_collection.find({ "topics": topic })
