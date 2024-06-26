c7b9be53e98580241bb6818c1ff2e9ac0ab6893a
Frandy Slueue <Frandy4ever@gmail.com>
2024-06-26 17:46:03 -0500

    Refactor: Add hooks for logging tests

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log correct total for 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log correct total for 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWithExactly('The total is: 20')).to.be.true;
+  });
+});
