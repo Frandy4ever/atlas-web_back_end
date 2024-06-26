a453c14067e0130bd24e04d51335379a1bb84926
Frandy Slueue <Frandy4ever@gmail.com>
2024-06-26 16:09:46 -0500

    Add calculateNumber function with SUM, SUBTRACT, and DIVIDE operations and tests

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,29 +1,43 @@
+const SUM = 'SUM';
+const SUBTRACT = 'SUBTRACT';
+const DIVIDE = 'DIVIDE';
+
+function isNegZero(n) {
+  n = Number(n);
+  return n === 0 && 1 / n === -Infinity;
+}
+
 /**
  * Performs a mathematical operation on two numbers after rounding them.
  *
  * @param {string} type - The type of operation to perform. Can be 'SUM', 'SUBTRACT', or 'DIVIDE'.
  * @param {number} a - The first number.
  * @param {number} b - The second number.
- * @returns {number|string} The result of the operation, or 'Error' if division by zero occurs.
+ * @returns {number|string} The result of the operation, or 'ERROR' if division by zero occurs.
  * @throws {TypeError} If any of the arguments are not the expected types.
  */
-module.exports = function calculateNumber(type, a, b) {
-  const numA = Number(a);
-  const numB = Number(b);
+module.exports = function calculateNumber(type, a, b = 0) {
+  let numA = Number(a);
+  let numB = Number(b);
 
-  if (Number.isNaN(numA) || Number.isNaN(numB)) {
-    throw new TypeError('Parameters must be numbers');
-  }
+  if (Number.isNaN(numA) || Number.isNaN(numB))
+    throw TypeError('Parameters must be numbers or able to coerce to number');
+
+  numA = Math.round(numA);
+  numB = Math.round(numB);
 
   switch (type) {
-    case 'SUM':
-      return Math.round(numA) + Math.round(numB);
-    case 'SUBTRACT':
-      return Math.round(numA) - Math.round(numB);
-    case 'DIVIDE':
-      if (Math.round(numB) === 0) return 'Error';
-      return Math.round(numA) / Math.round(numB);
+    case SUM:
+      return numA + numB;
+    case SUBTRACT:
+      return numA - numB;
+    case DIVIDE:
+      if (numB === 0) return 'ERROR';
+      const quotient = numA / numB;
+      return isNegZero(quotient) ? 0 : quotient;
     default:
-      throw new TypeError('Invalid operation type');
+      throw Error(
+        'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
+      );
   }
 };
